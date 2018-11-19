5a6e4534121f2293a4def5f4910b4d55c9abc82f
Malamuty <sanderdrupal@gmail.com>
2018-11-19 13:35:37 +0100

    - Added Camera Controller - Added Destoying when out of camera bounds

---
+++ Couch_Game_Group4/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform Target;
+    public float SmoothingSpeed = 0.4f;
+
+    [Header("Offsets")]
+    public float HorizontalOffset = 2f;
+    public float VerticalOffset = -10;
+
+    private Vector3 _cameraMovingVelocity = Vector3.zero;
+
+
+    void LateUpdate() {
+        SmoothFollowTarget();        
+    }
+
+    private void SmoothFollowTarget() {
+        Vector3 targetPositionWithOffset = Target.position + new Vector3(HorizontalOffset, 0f, VerticalOffset);
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, targetPositionWithOffset, ref _cameraMovingVelocity, SmoothingSpeed);
+    }
+}
---
+++ Couch_Game_Group4/Assets/Scripts/KillWhenOutOfCameraBounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillWhenOutOfCameraBounds : MonoBehaviour {
+    public float BoundsOffset = 0.05f;
+    private Camera _mainCamera;
+
+    void Awake() {
+        _mainCamera = Camera.main;    
+    }
+
+    void Update() {
+        if (ObjectIsOutOfCameraBounds()) {
+            Destroy(this.gameObject);
+        }    
+    }
+
+    private bool ObjectIsOutOfCameraBounds() {
+        Vector2 ObjectScreenPosition = _mainCamera.WorldToViewportPoint(this.transform.position);
+
+        float leftBoundsWithOffset = 0f - BoundsOffset;
+        float rightBoundsWithOffset = 1f + BoundsOffset;
+
+        bool objectIsOutsideBounds = ObjectScreenPosition.x < leftBoundsWithOffset || ObjectScreenPosition.x > rightBoundsWithOffset;
+
+        return objectIsOutsideBounds;
+    }
+}
