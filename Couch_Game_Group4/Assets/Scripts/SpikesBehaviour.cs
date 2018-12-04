04075704448684a613a2ab6ff6fce358387a60fe
GianniFournier <43540865+GianniFournier@users.noreply.github.com>
2018-12-04 11:17:31 +0100

    Spikes scripted

---
+++ Couch_Game_Group4/Assets/Scripts/SpikesBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesBehaviour : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider t)
+    {
+        if (t.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("Fell into the spikes!");
+            Destroy(t);
+
+        }
+    }
+
+}
