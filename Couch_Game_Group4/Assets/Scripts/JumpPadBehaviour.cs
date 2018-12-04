7d43f5e3546fbbb0cd2e96e1fcb53cbf8cc0ca4a
GianniFournier <43540865+GianniFournier@users.noreply.github.com>
2018-12-04 11:08:47 +0100

    Jumppad fixes
    Fixed direction + code

---
+++ Couch_Game_Group4/Assets/Scripts/JumpPadBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private float _jumpPadPower;
+
+    [SerializeField]
+    private bool _isFacingUp;
+
+    [SerializeField]
+    private bool _isFacingLeft;
+
+    [SerializeField]
+    private bool _isFacingRight;
+
+
+    private void OnTriggerEnter(Collider t)
+    {
+        if (t.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("JumpPad used!");
+
+            if (_isFacingUp)
+            {
+                t.transform.position += new Vector3(0, _jumpPadPower, 0);
+            }
+
+            if (_isFacingRight)
+            {
+                t.transform.position += new Vector3(_jumpPadPower, 0, 0);
+            }
+
+            if (_isFacingLeft)
+            {
+                t.transform.position += new Vector3(-_jumpPadPower, 0, 0);
+            }
+
+
+        }
+    }
+
+
+
+}
