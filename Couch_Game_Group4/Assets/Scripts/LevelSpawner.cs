3ce23397df48853c33b3692eca397f820248a849
JDehaene <43540756+JDehaene@users.noreply.github.com>
2018-12-17 22:50:29 +0100

    Start LevelSpawn
    Start

---
+++ Couch_Game_Group4/Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour {
+
+    private GameObject[] _levelPieces;
+    private float _counter;
+    [SerializeField]
+    private Transform _bossLocation;
+    private Transform _startLocation;
+    
+    void Update ()
+    {
+        SpawnRandomPiece();
+	}
+    void SpawnRandomPiece()
+    {
+        _counter+=Time.deltaTime;
+        if(_counter >= 5)
+        {
+            int randomPiece = (int)(Random.Range(0, 3));
+            Instantiate(_levelPieces[randomPiece], _startLocation);
+            _counter = 0;
+        }
+            
+    }
+}
