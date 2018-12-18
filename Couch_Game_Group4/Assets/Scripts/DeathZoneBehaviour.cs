1f15820e3f0bb8b65dba933c9fe67c648dbd1709
GianniFournier <43540865+GianniFournier@users.noreply.github.com>
2018-12-18 10:37:01 +0100

    Boundries for Camera (Deathzone)
    Deathzone added so players die when they leave te screen (left)

---
+++ Couch_Game_Group4/Assets/Scripts/DeathZoneBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZoneBehaviour : MonoBehaviour
+{
+
+    void Update()
+    {
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider t)
+    {
+        if (t.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+
+            Destroy(t);
+
+        }
+        
+    }
+}
